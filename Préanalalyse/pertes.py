5e8c7e27bdd786bd25f67838fd392b819a161618
NathanBerube <naber260@ulaval.ca>
2024-12-01 17:00:55 -0500

    Create pertes.py

---
+++ Préanalalyse/pertes.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def delta_tlm(t_inf, t_in,t_out):
+    delta_sortant = t_inf - t_out
+    delta_entrant = t_inf - t_in
+    
+    return (delta_sortant - delta_entrant)/np.log(delta_sortant/delta_entrant)
+
+
+def calculer_pertes(r_tot, delta_tlm):
+    return  1/r_tot * delta_tlm
