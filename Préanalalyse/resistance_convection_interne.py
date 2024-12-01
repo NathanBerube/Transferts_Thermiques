74142136d870dcb55c494dd42d71881abf8ac5e9
Elias Ouellet-Oviedo <elias.ouellet-oviedo.1@ulaval.ca>
2024-12-01 15:33:45 -0500

    R_conv

--- Préanalalyse/resistance_convection_interne.py
+++ Préanalalyse/resistance_convection_interne.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-def résistance_convection_interne(D, k):
+def résistance_convection_interne(D, k, L):
     h = 3.66 * k / D
-    A = 1500 * np.pi * D / 4
+    A = L * np.pi * D / 4
 
     return 1/(h * A)
-
-print(résistance_convection_interne(0.2, 569*10**-3))
