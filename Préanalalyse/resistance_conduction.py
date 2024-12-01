15c668cb65aee1cefba3ead0b041cd218d23e484
YoanFF <yoan.francovic-fontaine.1@ulaval.ca>
2024-12-01 15:30:18 -0500

    R conduction

--- Préanalalyse/resistance_conduction.py
+++ Préanalalyse/resistance_conduction.py
@@ -1,4 +1,4 @@
 import math
 def calculer_Rconduction_cylindre(diametre_ext, diametre_int, longueur_tuyau, conductivité_thermique ):
     R = (math.log(diametre_ext-diametre_int))/(2*math.pi*longueur_tuyau*conductivité_thermique)
-    return()
+    return(R)
