7ac7ef107608737ad88286337a829465b9b5bb24
YoanFF <yoan.francovic-fontaine.1@ulaval.ca>
2024-12-01 15:41:47 -0500

    2 fois épaisseur

--- Préanalalyse/resistance_conduction.py
+++ Préanalalyse/resistance_conduction.py
@@ -1,6 +1,6 @@
 import math
 def calculer_Rconduction_cylindre(D_int, epaisseur, L_tuyau, C_thermique):
-    Ratio = (D_int + epaisseur)/D_int
+    Ratio = (D_int + (2*epaisseur))/D_int
 
     R = math.log(Ratio)/(2*math.pi*L_tuyau*C_thermique)
     
